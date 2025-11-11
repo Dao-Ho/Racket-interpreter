11761fa6674c1debca4eaa952b4625934b2f1d76
Dao Ho <84757503+Dao-Ho@users.noreply.github.com>
2025-11-11 17:57:28 -0500

    feat: REPL (#3)
    * feat: repl (read line by line + print tokens
    
    * feat: main using repl

---
+++ pkg/repl/repl.go
@@ -0,0 +1,28 @@
+package repl
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"racket-interpreter/pkg/lexer"
+	"racket-interpreter/pkg/token"
+)
+
+const PROMPT = ">> "
+
+func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+	for {
+		fmt.Print(PROMPT)
+		scanned := scanner.Scan()
+		if !scanned {
+			fmt.Fprintln(out, scanner.Err())
+			return
+		}
+		line := scanner.Text()
+		l := lexer.NewLexer(line)
+		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
+			fmt.Printf("%+v\n", tok)
+		}
+	}
+}
